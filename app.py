1124d7c5c3815a548ee008884ca4cd77d520088c
killswitch-gui <a.rymdekoharvey@gmail.com>
2017-03-05 17:28:01 -0500

    init

--- app.py
+++ app.py
@@ -109,5 +109,5 @@ def request_forward(request_path):
     return "works/n"
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 80))
     app.run(host='0.0.0.0', port=port)
